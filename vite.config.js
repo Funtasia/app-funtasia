ae679911a14e9d5d2be3515b4a6d3113acdc7b62
ffgtfgh <152868614+ffgtfgh53@users.noreply.github.com>
2026-06-06 20:32:20 +0800

    feat: add .env files to seperate dev server and prod server env variables

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
@@ -6,73 +6,72 @@ import { fileURLToPath, URL } from 'node:url';
 import { resolve } from 'path';
 import { compression } from 'vite-plugin-compression2'
 
-// for when hosting on github pages using project sites
-// const base = "/funtasia_app/"
-const base = ""
-
-export default defineConfig({
-  base:base,  
-  plugins: [
-    threeMinifier(), // 7kB saved
-    ViteMinifyPlugin({removeAttributeQuotes: true, removeComments: true, removeRedundantAttributes: true}), // 1kB saved
-    tailwindcss(),
-    compression(), // compresses files
-  ],
-  define: {
-    ASSETS_BASE_URL: JSON.stringify('https://cdn.jsdelivr.net/gh/garethlearnscoding/funtasia_assets@1.1.0'), // Remember to bump the version here when updating `funtasia_assets`!
-    VERSION: JSON.stringify('v6-8-5') // version number used for models
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)) // for resolution of imports in the js files
-    }
-  },
-  assetsInclude: ['**/*.glb'],
-  server: {
-    port:5317,
-    allowedHosts: ["chunky-toaster.seagull-hippocampus.ts.net","broken-toaster.seagull-hippocampus.ts.net"], // for dev testing, raw IP addresses work
-    host:true, // whether to host only on localhost (false) or on all addresses (true)
-  },
-  build: {
-    minify: "terser",
-    sourcemap: 'hidden',
-    terserOptions: {
-      toplevel: true,
+export default defineConfig( ({ mode }) => { 
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return {
+    base: process.env.VITE_BASE,
+    plugins: [
+      threeMinifier(), // 7kB saved
+      ViteMinifyPlugin({removeAttributeQuotes: true, removeComments: true, removeRedundantAttributes: true}), // 1kB saved
+      tailwindcss(),
+      compression(), // compresses files
+    ],
+    define: {
+      ASSETS_BASE_URL: JSON.stringify(process.env.VITE_ASSETS_BASE_URL),
+      VERSION: JSON.stringify(process.env.VITE_MODEL_VERSION),
     },
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        notfound404: resolve(__dirname, "404.html")
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)) // for resolution of imports in the js files
+      }
+    },
+    assetsInclude: ['**/*.glb'],
+    server: {
+      port:5317,
+      allowedHosts: ["chunky-toaster.seagull-hippocampus.ts.net","broken-toaster.seagull-hippocampus.ts.net"], // for dev testing, raw IP addresses work
+      host:true, // whether to host only on localhost (false) or on all addresses (true)
+    },
+    build: {
+      minify: "terser",
+      sourcemap: 'hidden',
+      terserOptions: {
+        toplevel: true,
       },
-      output: {
-        manualChunks(id) {
-          // split three.js into seperate chunk from other node_modules
-          if (id.includes('node_modules')) {
-            if (id.includes('three')) {
-              if (id.includes('GLTFLoader.js')) {
-                return 'vendor-three-gltf-loader';
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          notfound404: resolve(__dirname, "404.html")
+        },
+        output: {
+          manualChunks(id) {
+            // split three.js into seperate chunk from other node_modules
+            if (id.includes('node_modules')) {
+              if (id.includes('three')) {
+                if (id.includes('GLTFLoader.js')) {
+                  return 'vendor-three-gltf-loader';
+                }
+                if (id.includes('OrbitControls.js')) {
+                  return 'vendor-three-orbit-controls';
+                }
+                if (id.includes('three.module.js')) {
+                  return 'vendor-three-module';
+                }
+                if (id.includes('three.core.js')) {
+                  return 'vendor-three-core';
+                }
+                if (id.includes('troika-three')) {
+                  return 'vendor-troika-three';
+                }
+                return 'vendor-three-other';
+              } 
+              if (id.includes('html5-qrcode')) {
+                if (id.includes('zxing-js')) {
+                  return 'vendor-html5-qrcode-xzing-js';
+                }
+                return 'vendor-html5-qrcode';
               }
-              if (id.includes('OrbitControls.js')) {
-                return 'vendor-three-orbit-controls';
-              }
-              if (id.includes('three.module.js')) {
-                return 'vendor-three-module';
-              }
-              if (id.includes('three.core.js')) {
-                return 'vendor-three-core';
-              }
-              if (id.includes('troika-three')) {
-                return 'vendor-troika-three';
-              }
-              return 'vendor-three-other';
-            } 
-            if (id.includes('html5-qrcode')) {
-              if (id.includes('zxing-js')) {
-                return 'vendor-html5-qrcode-xzing-js';
-              }
-              return 'vendor-html5-qrcode';
+              return 'vendor-other';
             }
-            return 'vendor-other';
           }
         }
       }
